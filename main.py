311d1eeb7815b7e52dd476729d336cc8c822bf80
Brise Kael <brise.kael@outlook.com>
2018-05-02 13:04:41 +0800

    Update main.py

--- main.py
+++ main.py
@@ -8,46 +8,89 @@ Created on Thu Apr 26 14:17:14 2018
 import helper
 import numpy as np
 
+# step 1, get all word and its counts, sort by word frequency
+# we get word_count_list
 
 strategy_instance = helper.strategy()
+word_count_dict = dict()
+
+for words in strategy_instance.class0:
+    for word in words:
+        if word in word_count_dict:
+            word_count_dict[word] += 1
+        else:
+            word_count_dict[word] = 1
+            
+word_count_list = sorted(word_count_dict.items(), key=lambda x: x[1], reverse=True)
+
+## step 2, get word and its index
+# the smaller the word freq, the smaller the word index
+# add 'UNK' as unknown word into the front
+# we get dictionary
 
 dictionary = dict()
 dictionary['UNK'] = 0
-for words in strategy_instance.class0:
-    for word in words:
-        if word not in dictionary:
-            dictionary[word] = len(dictionary)
+for word, count in word_count_list:
+    dictionary[word] = len(dictionary)
+    
+#### step 1-2, testing sort freq does matter
+#### it does matter. 
+#    
+#dictionary = dict()
+#dictionary['UNK'] = 0
+#
+#for words in strategy_instance.class0:
+#    for word in words:
+#        if word not in dictionary:
+#            dictionary[word] = len(dictionary)
+#
+#            
+#for words in strategy_instance.class1:
+#    for word in words:
+#        if word not in dictionary:
+#            dictionary[word] = len(dictionary)
 
-for words in strategy_instance.class1:
-    for word in words:
-        if word not in dictionary:
-            dictionary[word] = len(dictionary)
-
-reversed_dictionary = dict(zip(dictionary.values(), dictionary.keys()))
+### step 3, x_train is the train data with word index sort by freq
+# and its counts in this sentence
+# idx   [0, 1, 2, ..., len(dictionary)]
+# count [0, 9, 5, ..., 0, 0, 0]
+# we get x_train, y_train as np.array
 
 x_train = []
 y_train = []
 for words in strategy_instance.class0:
     tokens = [0] * len(dictionary)
     for word in words:
-        tokens[dictionary[word]] = words.count(word) ## 1
+        if word in dictionary:
+            tokens[dictionary[word]] = words.count(word) ## 1
+        else:
+            tokens[dictionary['UNK']] += 1
     x_train.append(tokens)
     y_train.append(0)
 
 for words in strategy_instance.class1:
     tokens = [0] * len(dictionary)
     for word in words:
-        tokens[dictionary[word]] = words.count(word) ## 1
+        if word in dictionary:
+            tokens[dictionary[word]] = words.count(word) ## 1
+        else:
+            tokens[dictionary['UNK']] += 1
     x_train.append(tokens)
     y_train.append(1)
 
 x_train = np.array(x_train)
 y_train = np.array(y_train)
 
-parameters={'gamma': 'auto', 'C': 1.0, 'kernel': 'linear', 'degree': 3, 'coef0': 0.0}
+## step 5, train with x_train, y_train
+# 
 
-#clf = strategy_instance.train_svm(parameters, x_train, y_train)
-#print('training_score:', clf.score(x_train, y_train))
+parameters={'gamma': 'auto', 'C': 1, 'kernel': 'rbf', 'degree': 3, 'coef0': 0.0}
+
+clf = strategy_instance.train_svm(parameters, x_train, y_train)
+print('training_score:', clf.score(x_train, y_train))
+
+# step 6, get x_test, y_test
+#
 
 with open('test_data.txt', 'r') as test_class1_file:
     test_class1 = [line.strip().split(' ') for line in test_class1_file]
@@ -59,37 +102,15 @@ for words in test_class1:
     for word in words:
         if word in dictionary:
             tokens[dictionary[word]] = words.count(word) ## 1
+        else:
+            tokens[dictionary['UNK']] += 1
     x_test.append(tokens)
     y_test.append(1)
 
 x_test = np.array(x_test)
 y_test = np.array(y_test)
 
-
-#print('testing_score:', clf.score(x_test, y_test))
-
-training_data = []
-for tokens in strategy_instance.class0:
-    training_data += [' '.join(tokens)]
-
-for tokens in strategy_instance.class1:
-    training_data += [' '.join(tokens)]
-
-
-## 
-from sklearn.feature_extraction.text import TfidfVectorizer
-vectorizer = TfidfVectorizer(min_df=1)
-x_train_gai = vectorizer.fit_transform(training_data)
-
-##
-clf2 = strategy_instance.train_svm(parameters, x_train_gai, y_train)
-print('training_score_2:', clf2.score(x_train_gai, y_train))
-# 
-with open('test_data.txt', 'r') as test_class1_file:
-    testing_data = [line.strip() for line in test_class1_file]
-x_test_gai = vectorizer.transform(testing_data)
-
-print('testing_score_2:', clf2.score(x_test_gai, y_test))
+print('testing_score:', clf.score(x_test, y_test))
 
 # =============================================================================
 # model selection
@@ -97,6 +118,7 @@ print('testing_score_2:', clf2.score(x_test_gai, y_test))
 # =============================================================================
 from sklearn.model_selection import GridSearchCV
 from sklearn.svm import SVC
+from sklearn.model_selection import cross_val_score
 
 # =============================================================================
 # parameters={'gamma': 'auto', 'C': 1.0, 'kernel': 'linear', 'degree': 3, 'coef0': 0.0}
@@ -108,14 +130,18 @@ from sklearn.svm import SVC
 # =============================================================================
 # kernel=linear
 # C=10, test_score=0.45
-model = SVC(kernel='linear')
-#param_grid = {'C': [1e-3, 1e-2, 1e-1, 1, 10, 100, 1000]}
-param_grid = {'C': range(1, 15)}
+#model = SVC(kernel='linear')
+##param_grid = {'C': [1e-3, 1e-2, 1e-1, 1, 10, 100, 1000]}
+##param_grid = {'C': [0.01, 0.1, 1, 10, 100]} => 0.01
+##param_grid = {'C': [0.0001, 0.001, 0.01, 0.1, 1]} => 0.001
+#param_grid = {'C': [0.00001, 0.0001, 0.001, 0.01, 0.1]}
+#
+#grid_search = GridSearchCV(model, param_grid, verbose=2, cv=10)
+#grid_search.fit(x_train, y_train)
+#
+#print(grid_search.best_estimator_)
 
-grid_search = GridSearchCV(model, param_grid, verbose=2, cv=10)
-grid_search.fit(x_train_gai, y_train)
 
-print(grid_search.best_estimator_)
 
 # =============================================================================
 # kernel=poly
@@ -136,13 +162,15 @@ print(grid_search.best_estimator_)
 # kernel=rbf
 # C
 # gamma
+
 #model = SVC(kernel='rbf')
-#param_grid = {'C': [1e-3, 1e-2, 1e-1, 1, 10, 100, 1000], 'gamma': [0.001, 0.0001]} 
+#param_grid = {'C': [0.1, 1, 10, 100], 
+#              'gamma': [0.1, 1, 10, 100]} 
 #
-#grid_search = GridSearchCV(model, param_grid, n_jobs = 8, verbose=1)    
-#grid_search.fit(x_train_gai, y_train)
+#grid_search = GridSearchCV(model, param_grid, verbose=2, cv=5)    
+#grid_search.fit(x_train, y_train)
 #
-#print(model.best_estimator_)
+#print(grid_search.best_estimator_)
 
 
 # =============================================================================
@@ -150,11 +178,5 @@ print(grid_search.best_estimator_)
 # C
 # gamma
 # coef0
-
-
-
-
-
-
 
 
